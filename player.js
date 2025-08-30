e6f23c7a0d1bceabfe1f9d366bf119e5942a4a7f
RIFKI MAULANA <146139031+rifkimaulanaa@users.noreply.github.com>
2025-08-30 15:17:52 +0700

    Update player.js

--- player.js
+++ player.js
@@ -102,7 +102,7 @@ function initializePlayer(client) {
                 iconURL: musicIcons.playerIcon,
                 url: config.SupportServer
             })
-            .setFooter({ text: `Developed by SSRR | Prime Music v1.2`, iconURL: musicIcons.heartIcon })
+            .setFooter({ text: `Developed by Rifki | Prime Music v1.2`, iconURL: musicIcons.heartIcon })
             .setTimestamp()
             .setDescription(  
                 `- **Title:** [${track.info.title}](${track.info.uri})\n` +
@@ -512,3 +512,4 @@ function createActionRow2(disabled) {
 }
 
 module.exports = { initializePlayer };
+
